3d0531383a6e2e06a3f088077d33923013ea1885
Richard Bielko <richard.bielko@gmail.com>
2023-04-21 19:25:34 +0200

    start new project Text_RPG_game

---
+++ Text_RPG_game/src/game_logic/GameLogic.java
@@ -0,0 +1,19 @@
+package game_logic;
+
+import java.util.Scanner;
+
+public class GameLogic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // get input method
+       public int getInput(String prompt)
+
+        // "new screen" method - prints a lot of new lines to clear the console
+
+        // print separator method "----------------"
+
+        // print a head
+
+        // wait for input to continue
+    }
+}
